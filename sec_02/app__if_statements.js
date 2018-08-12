abe565f96abdfba9ae4792bdbe9329e66aec5592
gw <gunther.weissenbaeck@muellerboon.de>
2018-08-12 11:39:03 +0200

    added if statements

---
+++ sec_02/app__if_statements.js
@@ -0,0 +1,75 @@
+/*IF STATEMENTS*/
+
+const id=100
+
+/*EQUAL*/
+/*if (id == 100) {
+    console.log('id is 100')
+}else {
+    console.log('id is not 100')
+}*/
+
+/*NOT EQUAL*/
+/*if (id!= 500){
+    console.log('id is not 500')
+}
+*/
+
+/*EQUAL VALUE AND TYPE*/
+/*if (id === 100) {
+    console.log('id is correct')
+}else {
+    console.log('id is not correct')
+}
+*/
+
+/*TEST IF VARIABLE EXISTS*/
+/*if(typeof id !== 'undefined'){
+    console.log(`the id is ${id}.`)
+}else {
+    console.log('no id')
+}*/
+
+/*GREATER OR LESS THEN*/
+/*if (id<=50) {
+    console.log('id is less then 50')
+}else {
+    console.log('id is greater then 50')
+}
+*/
+
+/*IF ELSE*/
+/*const color = 'yellow'
+
+if (color === 'red') {
+    console.log('the color is red')
+}else {
+    console.log('the color is not red')
+}
+*/
+
+/*LOCAL OPERATORS*/
+// const name = 'steve';
+// const age = 25;
+
+// and &&
+/*if (age>0 && age <12) {
+    console.log(`${name} is a child`);
+}else if (age>=12 && age <=19) {
+    console.log(`${name} is a teenager`);
+}else {
+    console.log(`${name} is an adult`);
+}
+*/
+
+// or ||
+/*if (age<16 || age>65) {
+    console.log(`${name} cannot enter the race!`)
+}else {
+    console.log(`${name} can enter the race!`)
+}
+*/
+
+/*TERNARY OPERATOR*/
+// console.log(id === 100 ? 'correct' : 'incorrect');
+
